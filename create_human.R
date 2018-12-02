4df5a85844f1aaab96e668f4600f4591976c8ec2
pyrysipila <44614780+pyrysipila@users.noreply.github.com>
2018-12-03 01:06:12 +0200

    Update create_human.R

--- create_human.R
+++ create_human.R
@@ -1,4 +1,4 @@
-#Pyry Sipil‰
+#Pyry Sipil√§
 
 #2 Read the data
 
@@ -50,3 +50,73 @@ human <- inner_join(hd, gii, by="Country")
 glimpse(human)
 
 write.csv(human, file="//atkk/home/p/pyrysipi/Documents/Tutkimus/Kurssit/Introduction to Open Data Science 2018/Data/human.csv",row.names = FALSE)
+
+
+#### RStudio Exercise 5, data wrangling
+
+?read.table
+human <- read.csv("http://s3.amazonaws.com/assets.datacamp.com/production/course_2218/datasets/human1.txt" , sep = "," , header = T)
+head(human)
+names(human)
+str(human)
+dim(human)
+summary(human)
+
+# Original data from: http://hdr.undp.org/en/content/human-development-index-hdi
+
+# The data are from the United Nations Development Programme. They describe most countries in the world.
+
+#The variables are:
+
+# HDI = Human.Development.Index..HDI.
+# Life.Exp = Life.Expectancy.at.Birth
+# Edu.Exp = Expected.Years.of.Education
+# Edu.Mean = Mean.Years.of.Education
+# GNI = Gross.National.Income..GNI..per.Capita
+# GNI.Minus.Rank = GNI.per.Capita.Rank.Minus.HDI.Rank
+
+# GII = Gender.Inequality.Index..GII.
+# GII.Rank = Rank of Gii
+# Mat.Mor = Maternal.Mortality.Ratio
+# Ado.Birth = Adolescent.Birth.Rate
+# Parli.F = Percent.Representation.in.Parliament
+# Edu2.F = Population.with.Secondary.Education..Female.
+# Edu2.M = Population.with.Secondary.Education..Male.
+# Edu2.FM = Female to male ratio in the proportion of populaiton with secondary education
+# Labo.F = Labour.Force.Participation.Rate..Female.
+# Labo.M = Labour.Force.Participation.Rate..Male.
+
+#1
+library(stringr)
+str_replace(human$GNI, pattern=",", replace ="")
+human <- mutate(human, GNI = as.numeric(GNI))
+
+#2
+keep <- c("Country", "Edu2.FM", "Labo.FM", "Edu.Exp", "Life.Exp", "GNI", "Mat.Mor", "Ado.Birth", "Parli.F")
+human <- select(human,one_of(keep))
+
+#3
+# filter out all rows with NA values
+human <- filter(human, complete.cases(human) == TRUE)
+
+#4
+human$Country
+
+# look at the last 10 observations of human
+tail(human, n=10)
+
+# define the last indice we want to keep
+last <- nrow(human) - 7
+
+# choose everything until the last 7 observations
+human_ <- human[1:last, ]
+
+# add countries as rownames
+rownames(human_) <- human_$Country
+
+#5
+
+# remove the Country variable
+human_ <- select(human, -Country)
+
+write.csv(human, file="//atkk/home/p/pyrysipi/Documents/Tutkimus/Kurssit/Introduction to Open Data Science 2018/Data/human.csv",row.names = TRUE)
